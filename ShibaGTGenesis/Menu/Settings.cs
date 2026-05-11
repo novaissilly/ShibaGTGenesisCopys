85c77fabbba9860e80dda75ac4b31e774d3348d5
Novaafr <idk777800111@gmail.com>
2026-05-11 22:49:32 +0100

    move up a lil

--- ShibaGTGenesis/Menu/Settings.cs
+++ ShibaGTGenesis/Menu/Settings.cs
@@ -134,7 +134,7 @@ namespace ShibaGTGenesis.Menu
 
                 if (Menu.GetGunInput(true))
                 {
-                    Menu.Instance.textMesh.transform.position = Pointer.transform.position;
+                    Menu.Instance.textMesh.transform.position = Pointer.transform.position + new Vector3(0f, 0.6f, 0f);
                 }
             }
         }
